b4486f2b15b6f9903877c9be596e6eb09fc10da4
sara040616 <sara.sierraramos@gmail.com>
2023-10-30 10:19:27 -0500

    Ajustes

---
+++ src/pages/dashboard/components/AlertPost.jsx
@@ -0,0 +1,14 @@
+import Alert from 'react-bootstrap/Alert';
+
+function AlertPost() {
+  return (
+    <Alert variant="success">
+      <Alert.Heading>Agregado con exito</Alert.Heading>
+   
+  
+    
+    </Alert>
+  );
+}
+
+export default AlertPost;
